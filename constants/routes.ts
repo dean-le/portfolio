017fdd70bdd18c93fa3341574947ff340afba2d4
Dean Le <deanle.dev@gmail.com>
2023-06-10 15:53:48 +0700

    init project, setup chakra ui

---
+++ constants/routes.ts
@@ -0,0 +1,20 @@
+import { NavItem } from "@/types/routes";
+
+export const NAV_ITEMS: NavItem[] = [
+  {
+    label: "About me",
+    href: "#",
+  },
+  {
+    label: "Experience",
+    href: "#",
+  },
+  {
+    label: "Projects",
+    href: "#",
+  },
+  {
+    label: "Contact",
+    href: "#",
+  },
+];
